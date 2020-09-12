37836d92d3fde307ca9e8d988a358923355cc017
Hidde Wieringa <hidde@hiddewieringa.nl>
2020-09-12 19:37:13 +0200

    Use class syntax
    Ref https://github.com/kosmtik/kosmtik/pull/319

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ var path = require('path'),
     fs = require('fs'),
     yaml = require('js-yaml');
 
-var patchConfig = function (e) {
-    var filepath = path.join(e.project.root, 'bookmarks.yml');
-    if (fs.existsSync(filepath)) {
-        try {
-            e.options.places = yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
-            console.log('[Places] Loaded bookmarks from', filepath);
-        } catch (e) {
-            console.log('[Places] Unable to load bookmarks from', filepath);
-            e.options.places = [];
-        }
+class Places {
+    constructor(config) {
+        config.addJS('/node_modules/kosmtik-places/front.js');
+        config.addCSS('/node_modules/kosmtik-places/front.css');
+        config.on('project:tofront', this.patchConfig);
     }
-    e.options.places = e.options.places.concat(this.userConfig.bookmarks || []);
-};
 
-exports.Plugin = function (config) {
-    config.addJS('/node_modules/kosmtik-places/front.js');
-    config.addCSS('/node_modules/kosmtik-places/front.css');
-    config.on('project:tofront', patchConfig);
-};
+    patchConfig(e) {
+        var filepath = path.join(e.project.root, 'bookmarks.yml');
+        if (fs.existsSync(filepath)) {
+            try {
+                e.options.places = yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
+                console.log('[Places] Loaded bookmarks from', filepath);
+            } catch (e) {
+                console.log('[Places] Unable to load bookmarks from', filepath);
+                e.options.places = [];
+            }
+        }
+        e.options.places = e.options.places.concat(this.userConfig.bookmarks || []);
+    };
+}
+
+exports = module.exports = { Plugin: Places };
